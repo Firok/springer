a0bac0278b6434a40c0b07800ac36b3661c7b31c
Firok <batefirok@yahoo.fr>
2017-11-16 00:38:33 +0530

    Firok change

--- src/main/java/com/firok/springer/webservices/UserWebservice.java
+++ src/main/java/com/firok/springer/webservices/UserWebservice.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.firok.springer.entity.User;
 import com.firok.springer.services.UserService;
 
-@Controller
+@RestController
 @RequestMapping("users")
 public class UserWebservice {
 
